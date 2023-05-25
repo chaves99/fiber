b025afa260800a93103924fef51ebc8bb99aa38b
chaves99 <viniciusbaleia1999@gmail.com>
2023-05-25 18:09:31 -0300

    Some changes in the data structure.

--- src/main/java/com/fiber/repository/MealDayRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fiber.repository;
-
-import com.fiber.entity.MealDayEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MealDayRepository extends JpaRepository<MealDayEntity, Long> {
-}
